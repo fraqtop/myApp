6e826597bae46b884efce83038419395acb536b7
fraqtop <fraqtop@gmail.com>
2018-05-17 13:58:42 +0300

    anti spam time and presses checks, es6 uglifier

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Mail\ContactMail;
 use App\Post;
 use App\User;
+use Faker\Provider\DateTime;
 use Mail;
 use Illuminate\Http\Request;
 
@@ -35,17 +36,30 @@ class HomeController extends Controller
         return view('home');
     }
 
+    private function isSpam(Request $request)
+    {
+        $fakeField = $request->post('contactAdvanced');
+        $postingPeriod = (new \DateTime())->getTimestamp() - $request->post('contactTime');
+        $keyPressDiff = (int)$request->post('contactCounter') - strlen($request->post('contactMessage'));
+        if($fakeField != null || $postingPeriod < 5 || $keyPressDiff < 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
     public function contact(Request $request)
     {
         if ($request->isMethod('post'))
         {
-            if($request->post('contactAdvanced') != '')
+            if($this->isSpam($request))
             {
                 Mail::to(User::find(1))->send(new ContactMail('anti spam system',
                     'spam message was caught', ''));
             }
-            Mail::to(User::find(1))->send(new ContactMail($request->post('contactAuthor'),
-                $request->post('contactMessage'), $request->post('contactFeedback')));
+            else
+                Mail::to(User::find(1))->send(new ContactMail($request->post('contactAuthor'),
+                    $request->post('contactMessage'), $request->post('contactFeedback')));
             return redirect('/');
         }
         return view('contact');
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,13 +2,13 @@
 @section('content')
     <div class="auth-form">
         <div class="form-label">Let me know</div>
-        <form action="/contact" method="post">
+        <form id="contactForm" action="/contact" method="post">
             {{csrf_field()}}
             <div class="form-group">
                 <input class="form-control" type="text" placeholder="who are you" name="contactAuthor" required>
             </div>
             <div class="form-group">
-                <textarea class="form-control" placeholder="what do you want" name="contactMessage" required></textarea>
+                <textarea id="contactArea" class="form-control" placeholder="what do you want" name="contactMessage" required></textarea>
             </div>
             <div class="form-group">
                 <input class="form-control" type="email" placeholder="feedback" name="contactFeedback">
@@ -16,6 +16,7 @@
             <div class="form-group">
                 <input placeholder="advanced" class="form-control advanced" type="text" name="contactAdvanced">
             </div>
+            <input type="hidden" name="contactTime" value="{{(new DateTime())->getTimestamp()}}">
             <div class="form-group">
                 <input class="form-control btn" type="submit" value="send message">
             </div>
