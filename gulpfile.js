5d35e669c043faab984c0ec5de6447f98baaf32f
Roman Bukhantsov <fraqtop@gmail.com>
2020-04-26 21:09:15 +0300

    skills section is recreated, added aside menu with smooth scrolling between sections, added font-awesome icons

--- gulpfile.js
+++ gulpfile.js
@@ -3,62 +3,62 @@ const sass = require('gulp-sass');
 const csso = require('gulp-csso');
 const browserSync = require('browser-sync').create();
 const uglify = require('gulp-uglify-es').default;
-const concat = require ('gulp-concat');
+const concat = require('gulp-concat');
 const imagemin = require('gulp-imagemin');
 
 const basePath = 'resources/js/';
 const jsPaths = [
-        'node_modules/jquery/dist/jquery.js',
-        'node_modules/bootstrap/dist/js/bootstrap.js',
-        'node_modules/bootstrap/dist/js/bootstrap.bundle.js',
-        'node_modules/chart.js/dist/Chart.js',
-        'node_modules/jquery-ui-dist/jquery-ui.min.js',
-        `${basePath}particles.js`,
-        `${basePath}main.js`
-    ];
+  'node_modules/jquery/dist/jquery.js',
+  'node_modules/bootstrap/dist/js/bootstrap.js',
+  'node_modules/bootstrap/dist/js/bootstrap.bundle.js',
+  'node_modules/chart.js/dist/Chart.js',
+  'node_modules/jquery-ui-dist/jquery-ui.min.js',
+  `${basePath}particles.js`,
+  `${basePath}main.js`,
+  'node_modules/@fortawesome/fontawesome-free/js/all.js'
+];
 
-function css () {
-   return gulp.src('resources/sass/style.scss')
-            .pipe(sass())
-            .pipe(csso())
-            .pipe(gulp.dest('public/css'));
+function css() {
+  return gulp.src('resources/sass/style.scss')
+    .pipe(sass())
+    .pipe(csso())
+    .pipe(gulp.dest('public/css'));
 }
 
 function syncStart(done) {
-   browserSync.init({
-       proxy: '127.0.0.1'
-   });
-   done();
+  browserSync.init({
+    proxy: '127.0.0.1:8000'
+  });
+  done();
 }
 
-function syncReload(done)
-{
-    browserSync.reload();
-    done();
+function syncReload(done) {
+  browserSync.reload();
+  done();
 }
 
-function js () {
-   return gulp.src(jsPaths)
-       .pipe(concat('all.js'))
-       .pipe(uglify())
-       .pipe(gulp.dest('public/js'));
+function js() {
+  return gulp.src(jsPaths)
+    .pipe(concat('all.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/js'));
 }
 
-gulp.task('imgCompress', function (){
-   return gulp.src('resources/img/*')
-       .pipe(imagemin())
-       .pipe(gulp.dest('public/img'));
+gulp.task('imgCompress', function () {
+  return gulp.src('resources/img/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('public/img'));
 });
 
-function watch () {
-    gulp.watch('resources/sass/**/*.*', gulp.series(css));
-    gulp.watch([
-        'public/js/**/*.*',
-        'public/css/**/*.*',
-        'public/img/**/*.*',
-        'resources/views/**/*.*'
-    ], gulp.series(syncReload));
-    gulp.watch(basePath+'*', gulp.series(js))
+function watch() {
+  gulp.watch('resources/sass/**/*.*', gulp.series(css));
+  gulp.watch([
+    'public/js/**/*.*',
+    'public/css/**/*.*',
+    'public/img/**/*.*',
+    'resources/views/**/*.*'
+  ], gulp.series(syncReload));
+  gulp.watch(basePath + '*', gulp.series(js))
 }
 
 gulp.task('default', gulp.series(gulp.parallel(css, js, syncStart), watch));
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,110 +1,122 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset('css/style.css')}}?{{ sha1(file_get_contents(asset('css/style.css'))) }}">
-    <title>fraqtop</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="{{asset('css/style.css')}}">
+  <title>fraqtop</title>
 </head>
 <body>
-    <div class="pillow">
-        <header>
-            <div id="particles-js"></div>
-            <div class="hello-alert animated fadeIn">
-                <h1>romAn buhAncoV</h1>
-                <h4>web developer</h4>
-            </div>
-            <nav class="index-nav animated fadeInDown">
-                <a href="/posts">blog</a>
-                <a href="/contact">contact</a>
-                <a href="/football">football</a>
-            </nav>
-        </header>
+<div id="#navigation" class="pillow">
+  <header>
+    <div id="particles-js"></div>
+    <div class="hello-alert animated fadeIn">
+      <h1>romAn buhAncoV</h1>
+      <h4>web developer</h4>
     </div>
-    <div class="grid-container">
-        <section class="info">
-            <img src="{{$picture}}" style="opacity: 0.7">
-            <article>
-                Hi, my name is Roman, nice to meet you here. I'm web
-                developer based in Ukhta city. I like creating awesome and helpful things
-                using new technologies. Coding is one of my hobbies, which helps me
-                be happy even waking up at 6:00 on Monday.
-                I perform not only working tasks but my own projects too. It helps me
-                to improve development skills.
-                I try my code to be efficient, laconic and self-documenting.
-                I love structure, order and also stand for quality.
-                Coding is not the only thing, that keeps me in the good mood.
-                I'm crazy about different kinds of sport, especially football,
-                snowboarding and swimming.
-            </article>
-        </section>
-        <section class="timing">
-            <div class="graphic">
-                <div class="legend">
-                    <h3>Rarely</h3>
-                    <h3>Sometimes</h3>
-                    <h3>Often</h3>
-                    <h3>Usually</h3>
-                </div>
-                <div class="diagram">
-                    <div id="scss"><h3>scss</h3></div>
-                    <div id="t-sql"><h3>t-sql</h3></div>
-                    <div id="sharp"><h3>c#</h3></div>
-                    <div id="php"><h3>php</h3></div>
-                    <div id="python"><h3>python</h3></div>
-                    <div id="js"><h3>javascript</h3></div>
-                </div>
-            </div>
-            <div class="img-responsive">
-                <img src="{{URL::asset('img/pie.png')}}">
-            </div>
-        </section>
-        <section class="posts">
-            @foreach($posts as $post)
-                <a href="/posts/{{$post->id}}/">
-                    <div class="card post">
-                        <img class="card-img-top" src="{{$post->getPicture()}}">
-                            <div class="card-body">
-                            <h5 class="card-title">{{$post->title}}</h5>
-                            <p class="card-text">{{$post->content}}</p>
-                        </div>
-                        <div class="post-overflow">{{ $post->updated_at }}</div>
-                    </div>
-                </a>
-            @endforeach
-        </section>
+    <nav class="index-nav animated fadeInDown">
+      <a href="/posts">blog</a>
+      <a href="/contact">contact</a>
+      <a href="/football">football</a>
+    </nav>
+  </header>
+</div>
+<div class="sections-menu">
+  <div class="section-link" id="nav-link"><i class="fas fa-star"></i></div>
+  <div class="section-link" id="info-link"><i class="fas fa-star"></i></div>
+  <div class="section-link" id="knowledge-link"><i class="fas fa-star"></i></div>
+  <div class="section-link" id="posts-link"><i class="fas fa-star"></i></div>
+</div>
+<div class="grid-container">
+  <section class="info" id="info">
+    <div class="profile-image">
+      <img src="{{$picture}}">
     </div>
-    @include('footer')
+    <article>
+      Hi, my name is Roman, nice to meet you here. I'm web
+      developer from Moscow. I like creating awesome and helpful things
+      using new technologies. Coding is one of my hobbies, which helps me
+      be happy even waking up at 6:00 on Monday.
+      I perform not only working tasks but my own projects too. It helps me
+      to improve development skills.
+      I try my code to be efficient, laconic and self-documenting.
+      I love structure, order and also stand for quality.
+      Coding is not the only thing, that keeps me in the good mood.
+      I'm crazy about different kinds of sport, especially football,
+      snowboarding and swimming.
+    </article>
+  </section>
+  <section id="knowledge" class="p-2 container">
+    <div class="skills-container">
+      <div class="skill">
+        <i class="fab fa-php fa-2x" style="color: #6c8ef0"></i>
+        <div id="php" class="line"></div>
+      </div>
+      <div class="skill">
+        <i class="fab fa-python fa-2x" style="color: #DBC200;"></i>
+        <div id="python" class="line"></div>
+      </div>
+      <div class="skill">
+        <i class="fab fa-git fa-2x" style="color: #FF473D"></i>
+        <div id="git" class="line"></div>
+      </div>
+      <div class="skill">
+        <i class="fas fa-database fa-2x" style="color: #6c8ef0;"></i>
+        <div id="t-sql" class="line"></div>
+      </div>
+      <div class="skill">
+        <i class="fab fa-sass fa-2x" style="color: #FF8585"></i>
+        <div id="scss" class="line"></div>
+      </div>
+      <div class="skill">
+        <i class="fab fa-bootstrap fa-2x" style="color: #9000AD"></i>
+        <div id="bootstrap" class="line"></div>
+      </div>
+      <div class="skill">
+        <i class="fab fa-gulp fa-3x" style="color: #FF473D"></i>
+        <div id="gulp" class="line"></div>
+      </div>
+      <div class="skill">
+        <i class="fab fa-vuejs fa-2x" style="color: #00B344"></i>
+        <div id="vue" class="line"></div>
+      </div>
+      <div class="skill">
+        <i class="fab fa-js-square fa-2x" style="color: #DBC200"></i>
+        <div id="js" class="line"></div>
+      </div>
+      <div class="skill">
+        <i class="fab fa-laravel fa-2x" style="color: #FF473D"></i>
+        <div id="laravel" class="line"></div>
+      </div>
+    </div>
+  </section>
+  <section class="posts" id="posts">
+    @foreach($posts as $post)
+      <a href="/posts/{{$post->id}}/">
+        <div class="card post">
+          <img class="card-img-top" src="{{$post->getPicture()}}">
+          <div class="card-body">
+            <h5 class="card-title">{{$post->title}}</h5>
+            <p class="card-text">{{$post->content}}</p>
+          </div>
+          <div class="post-overflow">{{ $post->updated_at }}</div>
+        </div>
+      </a>
+    @endforeach
+  </section>
+</div>
+@include('footer')
 </body>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
-<script src="{{asset('js/all.js')}}?{{ sha1(file_get_contents(asset('js/all.js'))) }}"></script>
+<script src="{{asset('js/all.js')}}"></script>
 <script>
-    $(document).ready(function () {
-        particlesJS.load('particles-js', 'js/particles.json', function() {});
-        let infoSection = $(".info");
-        let timingSection = $(".timing");
-        let postsSection = $(".posts");
-        infoSection.css("visibility", "hidden");
-        timingSection.css("visibility", "hidden");
-        postsSection.css("visibility", "hidden");
-        let checkPoint = 350;
-        window.onscroll = function () {
-            if(infoSection.offset().top - window.pageYOffset < checkPoint)
-            {
-                infoSection.addClass("animated fadeInUp");
-                infoSection.css("visibility", "visible");
-            }
-            if(timingSection.offset().top - window.pageYOffset < checkPoint)
-            {
-                timingSection.addClass("animated zoomIn");
-                timingSection.css("visibility", "visible");
-            }
-            if(postsSection.offset().top - window.pageYOffset < checkPoint)
-            {
-                postsSection.addClass("animated zoomIn");
-                postsSection.css("visibility", "visible");
-            }
-        };
-    });
+  $(document).ready(function () {
+    let animationDuration = 1000
+    $('#nav-link').click(() => {$('html, body').animate({scrollTop: $('body').offset().top}, animationDuration);})
+    $('#info-link').click(() => {$('html, body').animate({scrollTop: $('#info').offset().top}, animationDuration);})
+    $('#knowledge-link').click(() => {$('html, body').animate({scrollTop: $('#knowledge').offset().top}, animationDuration);})
+    $('#posts-link').click(() => {$('html, body').animate({scrollTop: $('#posts').offset().top}, animationDuration);})
+  })
+  particlesJS.load('particles-js', 'js/particles.json', function () {});
 </script>
 </html>
